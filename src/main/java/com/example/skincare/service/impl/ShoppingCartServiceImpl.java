155d7f698436e046f397c1c110410786e9e0df46
angeladimovska <94134718+angeladimovska@users.noreply.github.com>
2022-03-10 22:06:02 +0100

    Update ShoppingCartServiceImpl.java

--- src/main/java/com/example/skincare/service/impl/ShoppingCartServiceImpl.java
+++ src/main/java/com/example/skincare/service/impl/ShoppingCartServiceImpl.java
@@ -35,7 +35,6 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
             if(!this.shoppingCartRepository.findById(cartId).isPresent())
                 throw new ShoppingCartNotFoundException(cartId);
 
-            //gi vrakjam site produkti vo kosnickata
             return this.shoppingCartRepository.findById(cartId).get().getProducts();
     }
 
@@ -47,7 +46,6 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
 
         return this.shoppingCartRepository.findByUserAndStatus(user,
                 ShoppingCartStatus.CREATED)
-                //sega else e ako nemam aktivna kosnicka da kreiram nova
                 .orElseGet(() ->{
                     ShoppingCart cart = new ShoppingCart(user);
                     return this.shoppingCartRepository.save(cart);
@@ -57,14 +55,10 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
     @Override
     public ShoppingCart addProductToShoppingCart(String username, Long productId) {
 
-       //prvo vidi dali ima aktivna kosnicka za korisnikot sho go davas preku username
         ShoppingCart shoppingCart=this.getActiveShoppingCart(username);
-
-        //koj mi e produktot shto sakam da go doadam
         Product product=this.productService.findById(productId)
                 .orElseThrow(()->new ProductNotFoundException(productId));
 
-        //dali produktot shto sakam da go dodadam veke postoi vo taa kosnicka
         if(shoppingCart.getProducts()
                 .stream().filter(p ->p.getId().equals(productId))
                 .collect(Collectors.toList()).size() > 0)
@@ -76,7 +70,6 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
 
     @Override
     public void deleteProductFromShoppingCart(String username, Long productId) {
-        //zemi ja aktivnata kosnicka za korisnikot
         ShoppingCart shoppingCart=this.getActiveShoppingCart(username);
     }
 }
