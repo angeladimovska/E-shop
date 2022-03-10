e7177ce4293eaa54d9569232d08ccb5fb5bf6287
angeladimovska <94134718+angeladimovska@users.noreply.github.com>
2022-03-10 21:58:22 +0100

    Update productAdd.js

--- frontend/src/components/Products/ProductAdd/productAdd.js
+++ frontend/src/components/Products/ProductAdd/productAdd.js
@@ -1,10 +1,8 @@
 import React, {useState} from 'react';
 import {useNavigate} from 'react-router-dom';
 
-//ovaa komponenta prakja post request
 const ProductAdd = (props) => {
 
-    //useHistory-MI RAI PROBLEM-NAMESTO TOA USENAVIGATE e za da mozam da redirektiram
     const navigate = useNavigate();
 
     const [formData, updateFormData] = useState({
@@ -15,18 +13,14 @@ const ProductAdd = (props) => {
         brand: 5
     })
 
-    //ova e e nekoj EVENT koj ke se kreira i treba da go ishendlame
     const handleChange = (e) => {
         updateFormData({
             ...formData,
-            //od eventot go zemame targetot i imeto na toj target toa ni e key i value e
             [e.target.name]: e.target.value.trim()
         })
     }
 
-    //form-event handler
     const onFormSubmit = (e) => {
-        //na ovoj event popreci mu go defaultnoto odnesuvanje- nemoj da gi  prakjas vednas podatocite tuku zastani
         e.preventDefault();
         const name = formData.name;
         const price = formData.price;
@@ -34,9 +28,7 @@ const ProductAdd = (props) => {
         const category = formData.category;
         const brand = formData.brand;
 
-        //category i brand se ids
         props.onAddProduct(name, price, quantity, category, brand);
-        //ova e redirectot!
         navigate("/products");
     }
 
