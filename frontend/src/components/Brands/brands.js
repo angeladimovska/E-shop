c7701eae95998bbe911b98aa984c537bd0cbc72b
angeladimovska <94134718+angeladimovska@users.noreply.github.com>
2022-03-10 21:56:19 +0100

    Update brands.js

--- frontend/src/components/Brands/brands.js
+++ frontend/src/components/Brands/brands.js
@@ -1,8 +1,4 @@
 import React from "react";
-
-//props e isto OBJEKT i e mnozina od properties koi moze da se predadat
-//toa shto ke imame vo props e toa sto ke predade nekoja parent komponenta kon ovaa
-//i vo slucajov brands e svojstvo na props zatoa pravis props.brands.map
 const brands = (props) =>{
     return (
         <div className={"container mb-4"}>
@@ -16,7 +12,6 @@ const brands = (props) =>{
                         </thead>
 
                         <tbody>
-                          {/*brands.MAP iterira niz listata toest e kako th:each="term : ${brands}" */}
                           {props.brands.map((term) => {
                               return (
                                   //vrati go imeto
@@ -34,5 +29,4 @@ const brands = (props) =>{
         </div>
     )
 }
-//da mozes da pristapuvas do brands nadvor od ovaa komponenta
 export default brands;
